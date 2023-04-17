aacff9891e98384a5574ebd102e4b8dd7c9080ed
Ekaterina Zhitkevich <zhitkevich.ekaterina@yandex.ru>
2023-04-17 21:42:53 +0300

    Initial commit

---
+++ AdapterPattern/src/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.pow(5,2));
+        System.out.println(calc.sum(10,15));
+        System.out.println(calc.mult(7,3));
+    }
+}
